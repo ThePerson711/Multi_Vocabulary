95e426ea3d1d201ce6a35b16fc0684055bdd37b9
ThePerson711 <147810054+ThePerson711@users.noreply.github.com>
2024-06-27 12:20:13 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -23,7 +23,6 @@ const re_inp = {
 const words_list = document.getElementById("words_show_list");
 let words = [];
 let ar_FT = []
-let tim_ar = [];
 let test_option = {
   lang: "kr-to-uz",
   num: 10,
@@ -41,21 +40,10 @@ let CurTest = {
   false: 0,
   num: 0
 }
-let settings = {
-    mode: "dark",
-    langTo: "kr-to-uz",
-    NumOfTests: 5,
-    Period: "all"
-};
 let CurentSelectedWord = 0;
-let CurentList = 1;
-let BeforeList = 1;
+let NumOfList = 7;
 if (localStorage.getItem("KR_VOC_MY_ffLS") !== null) {
   words = JSON.parse(localStorage.getItem("KR_VOC_MY_ffLS"));
-}
-
-if (localStorage.getItem("KR_VOC_MY_Setings") !== null) {
-    settings = JSON.parse(localStorage.getItem("KR_VOC_MY_Setings"));
 }
 
 /*
@@ -71,87 +59,61 @@ for (let i = 1; i <= 22; i++) {
 };
 SetDataToLS();*/
 
-Menu("test");
+Menu("words");
+//ClickedWord(45);
 //console.table(words[45-1].tested)
-//test.ask.style.visibility = "visible";
-//test.check.style.visibility = "hidden";
+test.ask.style.visibility = "visible";
+test.check.style.visibility = "hidden";
 //StartTest();
 //ClickedWord(1);
 
+TextToSpeech("", "kr");
+
+// ????????????????????????
+document.getElementById("test_select_type_test").click();
+StartTest();
+Menu("menu");
+
 //
-
-var setting_elements = document.querySelectorAll(".settings_inp");
-    setting_elements.forEach(element => {
-        element.addEventListener("change", () => {
-            console.log("_____gg");
-            if (document.getElementById("test_select_lang_kr_to_uz").checked) {
-                settings.langTo = "kr-to-uz";
-            } else if (document.getElementById("test_select_lang_uz_to_kr").checked = true) {
-                settings.langTo = "uz-to-kr";
-            }
-            if (document.getElementById("test_select_date_today").checked) {
-                settings.Period = "today";
-            } else if (document.getElementById("test_select_date_week").checked) {
-                settings.Period = "week";
-            } else if (document.getElementById("test_select_date_all").checked) {
-                settings.Period = "all";
-            }
-
-            settings.NumOfTests = document.getElementById("test_select_num").value;
-            SetSettingsToLS();
-    });    
-})
-
 function StartTest() {
-  if (document.getElementById("test_select_lang_kr_to_uz").checked) {
-    test_option.lang = "kr-to-uz";
-  } else if (document.getElementById("test_select_lang_uz_to_kr").checked) {
-    test_option.lang = "uz-to-kr";
-  }
-  if (document.getElementById("test_select_date_today").checked) {
-    test_option.date = "today";
-  } else if (document.getElementById("test_select_date_week").checked) {
-    test_option.date = "week";
-  } else if (document.getElementById("test_select_date_all").checked) {
-    test_option.date = "all";
-  }
-  test_option.num = (document.getElementById("test_select_num").value)
-  SlideScreenList(5);
-  CurentNumOfTest = 0;
-  CurTest.num = 0;
-  CurTest.true = 0;
-  CurTest.false = 0;
-  for (let i = 0; i < words.length; i++) {
-    if (test_option.date === "today") {
-      if ( daysPassedSince(words[i].date) <= 0) {
-        ar_FT.push(i);
-      }
-    } else if (test_option.date === "week") {
-      if ( daysPassedSince(words[i].date) <= 6) {
-        ar_FT.push(i);
-      }
-    } else {
-      ar_FT.push(i)
+  if (document.getElementById("test_select_type_card").checked) {
+    if (document.getElementById("test_select_lang_kr_to_uz").checked) {
+      test_option.lang = "kr-to-uz";
+    } else if (document.getElementById("test_select_lang_uz_to_kr").checked) {
+      test_option.lang = "uz-to-kr";
     }
-  }
-  //
-    tim_ar = [];
-    ar_FT.forEach(element => {
-        tim_ar.push({
-            ind: element,
-            fa: 0
-        })
-    });
-    tim_ar.forEach(element => {
-        fa_count_ = 0;
-        words[element.ind].tested.forEach(chech_el => {
-            if (chech_el !== true) {
-                fa_count_++;
-            }
-        });
-        element.fa = fa_count_;
-    });
+    if (document.getElementById("test_select_date_today").checked) {
+      test_option.date = "today";
+    } else if (document.getElementById("test_select_date_week").checked) {
+      test_option.date = "week";
+    } else if (document.getElementById("test_select_date_all").checked) {
+      test_option.date = "all";
+    }
+    test_option.num = (document.getElementById("test_select_num").value)
+    move_list.style = `transform: translate(-${4*100/NumOfList}%, 0);`;
+    CurentNumOfTest = 0;
+    CurTest.num = 0;
+    CurTest.true = 0;
+    CurTest.false = 0;
+    for (let i = 0; i < words.length; i++) {
+      if (test_option.date === "today") {
+        if ( daysPassedSince(words[i].date) <= 0) {
+          ar_FT.push(i);
+        }
+      } else if (test_option.date === "week") {
+        if ( daysPassedSince(words[i].date) <= 6) {
+          ar_FT.push(i);
+        }
+      } else {
+        ar_FT.push(i)
+      }
+    }
     NewTest();
+  }
+  if (document.getElementById("test_select_type_test").checked) {
+    //
+    move_list.style = `transform: translate(-${5*100/NumOfList}%, 0);`;
+  }
 }
 //
 function NewTest() {
@@ -162,9 +124,7 @@ function NewTest() {
     alert(`Test Finished\nNum${CurTest.num-1}\nTrue${CurTest.true}\nFalse${CurTest.false}`)
     Menu("test")
   } else {
-    CurentSelectedWord = //ar_FT[(Math.floor(Math.random()*ar_FT.length))];
-        SelectRand(tim_ar);
-
+    CurentSelectedWord = ar_FT[(Math.floor(Math.random()*ar_FT.length))];
     
     if (test_option.lang === "kr-to-uz") {
       document.getElementById("test_ask_word").innerHTML = 
@@ -182,8 +142,6 @@ function NewTest() {
         words[CurentSelectedWord].kr_word;
     }
   }
-  test.ask.style.visibility = "visible";
-  test.check.style.visibility = "hidden";
 }
 //
 function SoundBtn() {
@@ -192,7 +150,6 @@ function SoundBtn() {
   } else if (test_option === "uz-to-kr") {
     lang_ = "uz";
   }
-  console.log(words[CurentSelectedWord].kr_word)
   TextToSpeech(words[CurentSelectedWord].kr_word, "kr");
 }
 //
@@ -224,9 +181,6 @@ function CheckTheAnswer() {
 function SetDataToLS() {
   localStorage.setItem("KR_VOC_MY_ffLS", JSON.stringify(words));
 }
-function SetSettingsToLS() {
-    localStorage.setItem("KR_VOC_MY_Setings", JSON.stringify(settings))
-}
 //
 function ShowWords() {
   count = 0;
@@ -257,7 +211,7 @@ function ClickedWord(param1) {
   re_inp.uz.des.value = words[CurentSelectedWord].uz_des;
   document.getElementById("word_show_date").innerHTML =
     `Added date => ${words[CurentSelectedWord].date}`;
-    SlideScreenList(4);
+  move_list.style = `transform: translate(-${3*100/NumOfList}%, 0);`;
     var ans_ = {
       tr: 0,
       fa: 0
@@ -306,7 +260,7 @@ function ClickedWord(param1) {
 function DeleteWord() {
   words.splice(CurentSelectedWord, 1);
   SetDataToLS();
-  SlideScreenList(2);
+  move_list.style = `transform: translate(-${1*100/NumOfList}%, 0);`;
   ShowWords();
 }
 //
@@ -318,47 +272,25 @@ function ChangeWord() {
         words[CurentSelectedWord].kr_word = re_inp.kr.word.value;
         words[CurentSelectedWord].kr_des = re_inp.kr.des.value;
         SetDataToLS();
-        SlideScreenList(2);
+        move_list.style = `transform: translate(-${1*100/NumOfList}%, 0);`;
         ShowWords();
       }
 }
 //
-Menu("add")
-function SlideScreenList(num_of_list) {
-  BeforeList = CurentList;
-  CurentList = num_of_list;
-  move_list.style = `transform: translate(-${(num_of_list-1)*10}%, 0);`;
-}
-//
 function Menu(list_) {
   if (list_ === "add") {
-      SlideScreenList(1);
+    move_list.style = `transform: translate(0, 0);`;
       inp.kr.word.value = "";
       inp.kr.des.value = "";
       inp.uz.word.value = "";
       inp.uz.des.value = "";
   } else if (list_ === "words") {
-    SlideScreenList(2);
+    move_list.style = `transform: translate(-${1*100/NumOfList}%, 0);`;
     ShowWords();
   } else if (list_ === "test") {
-    SlideScreenList(3);
-    
-    //
-    document.getElementById("test_select_num").value = settings.NumOfTests;
-    if (settings.langTo === "kr-to-uz") {
-        document.getElementById("test_select_lang_kr_to_uz").checked = true;
-    } else if (settings.langTo === "uz-to-kr") {
-        document.getElementById("test_select_lang_uz_to_kr").checked = true;
-    }
-    if (settings.Period === "all") {
-        document.getElementById("test_select_date_all").checked = true;
-    } else if (settings.Period === "week") {
-        document.getElementById("test_select_date_week").checked = true;        
-    } else if (settings.Period === "today") {
-        document.getElementById("test_select_date_today").checked = true;        
-    }
-    //
-
+    move_list.style = `transform: translate(-${2*100/NumOfList}%, 0);`;
+  } else if (list_ === "menu") {
+    move_list.style = `transform: translate(-${6*100/NumOfList}%, 0);`;
   }
 }
 //
@@ -463,70 +395,19 @@ function getCurrentDateTime() {
         alert('Sorry, your browser does not support speech synthesis.');
       }
     }
-    //dsad
-    //
-    //console.table()
 
-    console.log("__")
-
-    var arrat = [null, 0, 0, 0, 0];
-    for (let counter_ = 0; counter_ < 1000; counter_++) {
-
-        arrat[SelectRand([
-            {ind: 1, fa: 20},
-            {ind: 2, fa: 15},
-            {ind: 3, fa: 10},
-            {ind: 4, fa: 5}
-        ])]++;
-        /*-console.table(SelectRand([
-            {ind: 1, fa: 20},
-            {ind: 2, fa: 15},
-            {ind: 3, fa: 10},
-            {ind: 4, fa: 5}
-        ]))    */
+    function CopyWords() {
+      navigator.clipboard.writeText(JSON.stringify(localStorage.getItem("KR_VOC_MY_ffLS")));
     }
-    console.table(arrat)
-    
 
-    function SelectRand(object_) {
-        answer_ = 0;
-        All_chechs_ = 0;
-        object_.forEach(element => {
-          All_chechs_+=element.fa;
-        });
-        tmAr = [];
-        object_.forEach(element => {
-          tmAr.push({ind: element.ind, 
-            kf: (element.fa/All_chechs_)})
-        });
-        tmAr.forEach(element => {
-          element.kf /= 2;
-          element.kf += ((0.5/object_.length));
-        });
-        //console.table(tmAr)
-        //console.table(tmAr);
-        for (let i = 1; i < tmAr.length; i++) {
-          tmAr[i].kf+=tmAr[i-1].kf;
-        }
-        Ran_Num_ = Math.random();
-        for (let i = tmAr.length-1; i >= 0;i--) {
-          if (tmAr[i].kf >= Ran_Num_) {
-            answer_ = tmAr[i].ind;
-          }
-        }
-        return answer_;
+    function SetAllWords() {
+      localStorage.setItem("KR_VOC_MY_ffLS",(JSON.parse(document.getElementById("textarea_for_setallwords").value)))
+    }
+
+    function ResetAllData() {
+      if (confirm('Are you sure you want to Reset all data?')) {
+        // Save it!
+        console.log('Thing was saved to the database.');
+        localStorage.removeItem("KR_VOC_MY_ffLS");
       }
-  
-
-      function myFunction(x) {
-        x.classList.toggle("change");
-      }
-
-
-      function GoToMenu() {
-        if (CurentList === 6) {
-          SlideScreenList(BeforeList);
-        } else {
-          SlideScreenList(6);
-        }
-      }
+    }
